8054c867648904e1a2affe9a23cfae80ffcb3df5
matthewdivdavis <davismatthewrocks@gmail.com>
2026-07-27 14:01:07 -0600

    Working on phase 5, almost done with the "logged out help" part

--- client/src/main/java/client/ClientMain.java
+++ client/src/main/java/client/ClientMain.java
@@ -2,9 +2,67 @@ package client;
 
 import chess.*;
 
+import java.io.PrintStream;
+import java.nio.charset.StandardCharsets;
+import java.util.Scanner;
+
+import static ui.EscapeSequences.*;
+
 public class ClientMain {
     public static void main(String[] args) {
         var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
         System.out.println("♕ 240 Chess Client: " + piece);
+        var out = new PrintStream(System.out, true, StandardCharsets.UTF_8);
+        preLogin(out);
     }
+    private static void preLogin(PrintStream out){
+        Scanner myScan = new Scanner(System.in);
+        out.println("Welcome to 240 Chess. Type 'help' to get started. ");
+        boolean loggedIn = false;
+        while(!loggedIn){
+            out.printf(SET_TEXT_COLOR_LIGHT_GREY);
+            if(loggedIn){
+                out.print("[LOGGED IN] >>> ");
+            }
+            else{
+                out.print("[LOGGED OUT] >>> ");
+            }
+            String input = myScan.next().trim();
+            if(input.equals("help")){
+                displayHelp(out);
+            }
+            else if(input.equals("login")) {
+                loggedIn = login(out);
+            } else if(input.equals("register")){
+                loggedIn = register(out);
+            } else if(input.equals("quit")){
+                return;
+            }
+
+        }
+    }
+    private static void displayHelp(PrintStream out){
+        out.printf("%s\tregister <USERNAME> <PASSWORD> <EMAIL> %s- to create an account\n", SET_TEXT_COLOR_BLUE, SET_TEXT_COLOR_MAGENTA);
+        out.printf("%s\tlogin <USERNAME> <PASSWORD> %s- to play chess\n", SET_TEXT_COLOR_BLUE, SET_TEXT_COLOR_MAGENTA);
+        out.printf("%s\tquit %s- playing chess\n", SET_TEXT_COLOR_BLUE, SET_TEXT_COLOR_MAGENTA);
+        out.printf("%s\thelp %s- with possible commands\n", SET_TEXT_COLOR_BLUE, SET_TEXT_COLOR_MAGENTA);
+        out.printf(SET_TEXT_COLOR_LIGHT_GREY);
+    }
+
+    private static boolean login(PrintStream out){
+        out.printf("%sInput your username and password (%s%susername password%s%s): ",SET_TEXT_COLOR_BLUE, SET_TEXT_COLOR_MAGENTA, SET_TEXT_ITALIC, RESET_TEXT_ITALIC, SET_TEXT_COLOR_BLUE);
+        Scanner myScan = new Scanner(System.in);
+        String username = myScan.next();
+        String password = myScan.next();
+        return true;
+    }
+    private static boolean register(PrintStream out){
+        out.printf("%sInput your username, password andm email (%s%susername password email%s%s): ",SET_TEXT_COLOR_BLUE, SET_TEXT_COLOR_MAGENTA, SET_TEXT_ITALIC, RESET_TEXT_ITALIC, SET_TEXT_COLOR_BLUE);
+        Scanner myScan = new Scanner(System.in);
+        String username = myScan.next();
+        String password = myScan.next();
+        String email = myScan.next();
+        return true;
+    }
+
 }
---
+++ client/src/main/java/client/ServerFacade.java
@@ -0,0 +1,13 @@
+package client;
+import java.io.PrintStream;
+import java.nio.charset.StandardCharsets;
+import java.sql.SQLSyntaxErrorException;
+import java.util.Scanner;
+
+import static ui.EscapeSequences.*;
+
+public class ServerFacade {
+    public static void main(String[] args){
+    }
+
+    }
