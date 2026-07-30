7c3ed0355a0cb88939ea1faa23ee9647887e7eef
matthewdivdavis <davismatthewrocks@gmail.com>
2026-07-30 11:40:25 -0600

    All my tests work?

--- server/src/main/java/dataaccess/DataAccess.java
+++ server/src/main/java/dataaccess/DataAccess.java
@@ -2,7 +2,6 @@ package dataaccess;
 
 import exception.ResponseException;
 import model.*;
-import org.eclipse.jetty.server.Authentication;
 
 public interface DataAccess {
     UserData addUser(UserData userData) throws ResponseException;
--- shared/src/main/java/exception/ResponseException.java
+++ shared/src/main/java/exception/ResponseException.java
@@ -3,7 +3,6 @@ package exception;
 import com.google.gson.Gson;
 
 import java.util.HashMap;
-import java.util.Map;
 
 public class ResponseException extends Exception {
 
